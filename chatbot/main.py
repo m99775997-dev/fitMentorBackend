69d7a6c74362612b3ad319c3d9507a78e1da22df
moham60 <alkingg289@gmail.com>
2026-09-16 17:29:17 +0300

    add vercelCors

--- chatbot/main.py
+++ chatbot/main.py
@@ -107,7 +107,7 @@ cors_origins = [
     "http://127.0.0.1:8080",
     "http://localhost:3000",
     "http://127.0.0.1:3000",
-    
+    "https://fit-mentor-nine.vercel.app",
 ]
 
 # لو في env override
